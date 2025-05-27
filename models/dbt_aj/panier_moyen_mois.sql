8a07e67186c0c7c8ccab23fef8372a51693029ef
antoine j <antoinejanalytics@gmail.com>
2025-05-27 12:16:10 +0000

    modif date

---
+++ models/dbt_aj/panier_moyen_mois.sql
@@ -0,0 +1,36 @@
+{{ config(materialized='table') }}
+
+{# Définition des sources pour bien qualifier les tables #}
+{% set orders     = source('localbike', 'orders') %}
+{% set order_items = source('localbike', 'order_items') %}
+
+WITH order_revenue AS (
+  SELECT
+    DATE_TRUNC(o.order_date, MONTH)       AS month,
+    oi.order_id,
+    ROUND(SUM(oi.quantity * oi.list_price),2)     AS revenue
+  FROM {{ orders }} AS o
+  JOIN {{ order_items }} AS oi
+    ON oi.order_id = o.order_id
+  GROUP BY
+    month,
+    oi.order_id
+),
+
+monthly_basket AS (
+  SELECT
+    month,
+    COUNT(DISTINCT order_id)                           AS nb_orders,
+    ROUND(SUM(revenue),2)                                       AS total_revenue,
+    ROUND(ROUND(SAFE_DIVIDE(SUM(revenue),2), COUNT(DISTINCT order_id)),2) AS avg_basket_value
+  FROM order_revenue
+  GROUP BY month
+)
+
+SELECT
+  month,
+  nb_orders,
+  total_revenue,
+  avg_basket_value
+FROM monthly_basket
+ORDER BY month
